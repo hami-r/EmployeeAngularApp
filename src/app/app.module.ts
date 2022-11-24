f1a2c9ef614c7165faeec02075c42fbbe7a7cee4
hami-r <mynameissaitama54@gmail.com>
2022-11-24 16:12:33 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,30 @@ import { EmployeeAddComponent } from './employee-add/employee-add.component';
 import { EmployeeSearchComponent } from './employee-search/employee-search.component';
 import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
 import { EmployeeDeleteComponent } from './employee-delete/employee-delete.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes = [
+  {
+    path:"",
+    component: AdminLoginComponent
+  },
+  {
+    path:"add",
+    component: EmployeeAddComponent
+  },
+  {
+    path: "search",
+    component: EmployeeSearchComponent
+  },
+  {
+    path: "edit",
+    component: EmployeeEditComponent
+  },
+  {
+    path: "delete",
+    component: EmployeeDeleteComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -20,7 +44,8 @@ import { EmployeeDeleteComponent } from './employee-delete/employee-delete.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
