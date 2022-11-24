5998638167b4f66eac645308f141fced23e3f43d
hami-r <mynameissaitama54@gmail.com>
2022-11-24 15:53:34 +0530

    employee add component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent
+    AdminLoginComponent,
+    EmployeeAddComponent
   ],
   imports: [
     BrowserModule,
