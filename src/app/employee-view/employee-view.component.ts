b3659707032c5c855debb8128892409733687e82
hami-r <mynameissaitama54@gmail.com>
2022-11-25 10:59:36 +0530

    data binded

--- src/app/employee-view/employee-view.component.ts
+++ src/app/employee-view/employee-view.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-view.component.css']
 })
 export class EmployeeViewComponent {
+  data:any = [
+  {
+    "employeeCode":1000,
+    "name":"hai",
+    "designation":"Developer",
+    "salary":100000,
+    "phone" :9874563210,
+    "mail":"kjk@mail.com",
+    "company":"Nest",
+    "year":3
+  },
+  {
+    "employeeCode":1001,
+    "name":"Ram",
+    "designation":"Trainee",
+    "salary":20000,
+    "phone" :88974563210,
+    "mail":"kjk@mail.com",
+    "company":"SFO",
+    "year":1
+  },
+  {
+    "employeeCode":1002,
+    "name":"May",
+    "designation":"Doctor",
+    "salary":250000,
+    "phone" :9653210213,
+    "mail":"kjk@mail.com",
+    "company":"MIMS",
+    "year":5
+  },
+  {
+    "employeeCode":1003,
+    "name":"Sonal",
+    "designation":"Manager",
+    "salary":35000,
+    "phone" :7012345872,
+    "mail":"kjk@mail.com",
+    "company":"SFO",
+    "year":3
+  },
+  {
+    "employeeCode":1004,
+    "name":"hai",
+    "designation":"Lead Engineer",
+    "salary":500000,
+    "phone" :98998999,
+    "mail":"kjk@mail.com",
+    "company":"Nest",
+    "year":15
+  }
+]
 
 }
